02403d1b49119f766938a74a26347c09556c90c3
pbrosamond <pbrosamond@gmail.com>
2024-02-16 00:37:32 -0800

    created second section of fruit intake

--- src/pages/FruitIntakePage/FruitIntakePage.js
+++ src/pages/FruitIntakePage/FruitIntakePage.js
@@ -8,7 +8,7 @@ function FruitIntakePage() {
 
       <form className='main__form1'>
         <div className='main__box1'>
-          <label for="dropdownField">
+          <label htmlFor="dropdownField">
             <p className='main__label'>vintage year</p>
           </label>
           <select className='main__dropdown' id="dropdownField" name="dropdownField">
@@ -20,7 +20,7 @@ function FruitIntakePage() {
         </div>
 
         <div className='main__box2'>
-          <label for="dropdownField">
+          <label htmlFor="dropdownField">
             <p className='main__label'>grower</p>
           </label>
           <select className='main__dropdown' id="dropdownField" name="dropdownField">
@@ -32,7 +32,7 @@ function FruitIntakePage() {
         </div>
 
         <div className='main__box3'>
-          <label for="dropdownField">
+          <label htmlFor="dropdownField">
             <p className='main__label'>varietal</p>
           </label>
           <select className='main__dropdown' id="dropdownField" name="dropdownField">
@@ -44,7 +44,7 @@ function FruitIntakePage() {
         </div>
 
         <div className='main__box4'>
-          <label for="dropdownField">
+          <label htmlFor="dropdownField">
             <p className='main__label'>vineyard</p>
           </label>
           <select className='main__dropdown' id="dropdownField" name="dropdownField">
@@ -56,7 +56,7 @@ function FruitIntakePage() {
         </div>
 
         <div className='main__box5'>
-          <label for="dropdownField">
+          <label htmlFor="dropdownField">
             <p className='main__label'>block</p>
           </label>
           <select className='main__dropdown' id="dropdownField" name="dropdownField">
@@ -68,7 +68,7 @@ function FruitIntakePage() {
         </div>
 
         <div className='main__box6'>
-          <label for="dropdownField">
+          <label htmlFor="dropdownField">
             <p className='main__label'>row</p>
           </label>
           <select className='main__dropdown' id="dropdownField" name="dropdownField">
@@ -85,9 +85,9 @@ function FruitIntakePage() {
 
       <h1 className='main__title'>add new intake</h1>
 
-      <form className='main__form2'>
+      <form className='main__form2 main__form2__row3'>
         <div className='main__box7'>
-          <label for="filterDropdownField" />
+          <label htmlFor="filterDropdownField" />
           <select className='main__dropdown' id="filterDropdownField" name="filterDropdownField">
             <option value="" disabled selected>filter dockets</option>
             <option value="all">All</option>
@@ -98,20 +98,31 @@ function FruitIntakePage() {
           </select>
         </div>
 
-        <div className='main__box8'>
-          <label for="dropdownField">
+        <section className='main__dockets'>
+          <p className='main__docket'>docket 1</p>
+          <p className='main__docket'>docket 2</p>
+          <p className='main__docket'>docket 3</p>
+          <p className='main__docket'>docket 4</p>
+          <p className='main__docket'>docket 5</p>
+          <p className='main__docket'>docket 6</p>
+          <p className='main__docket'>docket 7</p>
+          <p className='main__docket'>docket 8</p>
+        </section>
+
+        <div className='main__box8 box-margin'>
+          <label htmlFor="dropdownField">
             <p className='main__label'>date</p>
           </label>
           <select className='main__dropdown' id="dropdownField" name="dropdownField">
-            <option value="" disabled selected />
+            <option value="today's date" disabled selected>01 | 03 | 2024</option>
             <option value="option1">Option 1</option>
             <option value="option2">Option 2</option>
             <option value="option3">Option 3</option>
           </select>
         </div>
 
-        <div className='main__box9'>
-          <label for="dropdownField">
+        <div className='main__box9 box-margin'>
+          <label htmlFor="dropdownField">
             <p className='main__label'>bins</p>
           </label>
           <select className='main__dropdown' id="dropdownField" name="dropdownField">
@@ -122,8 +133,8 @@ function FruitIntakePage() {
           </select>
         </div>
 
-        <div className='main__box10'>
-          <label for="dropdownField">
+        <div className='main__box10 box-margin'>
+          <label htmlFor="dropdownField">
             <p className='main__label'>total weight</p>
           </label>
           <select className='main__dropdown' id="dropdownField" name="dropdownField">
@@ -134,8 +145,8 @@ function FruitIntakePage() {
           </select>
         </div>
 
-        <div className='main__box11'>
-          <label for="dropdownField">
+        <div className='main__box11 box-margin'>
+          <label htmlFor="dropdownField">
             <p className='main__label'>tare weight</p>
           </label>
           <select className='main__dropdown' id="dropdownField" name="dropdownField">
