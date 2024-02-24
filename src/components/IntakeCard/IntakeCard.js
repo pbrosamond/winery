405aff91208b98c357b19ef59438b74bb70a0828
pbrosamond <pbrosamond@gmail.com>
2024-02-24 00:50:46 -0800

    created popup for delete and set up delete icon click

--- src/components/IntakeCard/IntakeCard.js
+++ src/components/IntakeCard/IntakeCard.js
@@ -4,8 +4,27 @@ import editIcon from '../../assets/icons/editIcon.svg'
 import deleteIcon from '../../assets/icons/deleteIcon.svg'
 
 import formatDate from '../../utils/formatDate';
+import React, { useState, useEffect } from "react";
 
 function IntakeCard({intake}) {
+
+  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
+
+  const handleDeleteClick = () => {
+    setShowDeleteConfirmation(true);
+  };
+
+  const handleDeleteConfirmation = (confirmed) => {
+    if (confirmed) {
+      // Perform delete action here, e.g., call a delete API or update state
+      console.log(`Intake ID ${intake.intake_id} will be deleted.`);
+    }
+
+    // Close the delete confirmation pop-up
+    setShowDeleteConfirmation(false);
+  };
+
+
     return (
         <section className='intake__subcontainer'>
         <div className='intake__intake--border'>
@@ -47,8 +66,20 @@ function IntakeCard({intake}) {
 
         <section className='intake__icons'>
           <img className='intake_icon' src={editIcon} alt='Edit Icon'/>
-          <img className='intake_icon' src={deleteIcon} alt='Delete Icon'/>
+          <img className='intake_icon' src={deleteIcon} alt='Delete Icon' onClick={handleDeleteClick}/>
         </section>
+
+        {showDeleteConfirmation && <div className='intake__overlay' />}
+
+        {showDeleteConfirmation && (
+        <div className='intake__delete'>
+          <p className='intake__delete--text'>are you sure you want to delete intake {intake.intake_id}?</p>
+          <div className='intake__buttons'>
+            <button className="intake__button--delete" onClick={() => handleDeleteConfirmation(true)}>delete</button>
+            <button className="intake__button--cancel" onClick={() => handleDeleteConfirmation(false)}>cancel</button>
+          </div>
+        </div>
+      )}
 
       </section>
     );
